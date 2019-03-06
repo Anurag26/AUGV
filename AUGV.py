a1beb57b6845d344d12ae69a7e32a11d204feebb
Anurag Bannur <bannuranurag@gmail.com>
2019-03-06 14:27:47 +0530

    Update AUGV.py

--- AUGV.py
+++ AUGV.py
@@ -1,50 +1,123 @@
-
-# coding: utf-8
-
-# In[ ]:
-
-
 from firebase import firebase 
 import time
 from datetime import datetime 
 import schedule 
 i = 5 
 UUID=[]
+TotDistance=""
+IndividualStepDistance=""
+Instructions=""
+ManeuverBearings=""
+ManeuverLongLat=""
+TotalSteps=""
+
+
 app = firebase.FirebaseApplication('https://augv-c7210.firebaseio.com/',None)
-def job():
-    result = app.get('/json','uuid')
-    #print(result,str(datetime.now()))
-    return result
+# =============================================================================
+# def job():
+#     print("Job is running")
+#     result = app.get('/json','uuid')
+#     #print(result,str(datetime.now()))
+#     return result
+# 
+# =============================================================================
 
-def f():
+# =============================================================================
+# def f():
+#     print("F is running")
+#     schedule.every(1).seconds.do(job)
+#     print("Schedule while loop starting")
+#     while 1:
+#         schedule.run_pending()
+#         time.sleep(0)
+#         p=job()
+#         compareUUID(p)
+#         #l=[]
+#         #l.append(p)
+#         #print("P in f is",p)
+#         
+# =============================================================================
+# =============================================================================
+# def parseNewRouteData():
+#     print("new Route needs to parse")
+#     global k
+#     k="hello"
+#     app = firebase.FirebaseApplication('https://augv-c7210.firebaseio.com/',None)
+#     Routes = app.get('/json','routes')
+#     Legs = Routes['legs']
+#     global TotDistance
+#     TotDistance= Legs['distance']
+#     global IndividualStepDistance
+#     IndividualStepDistance= Legs['IndividualStepDistance']
+#     global Instructions
+#     Instructions= Legs['Instructions']
+#     global ManeuverBearings
+#     ManeuverBearings= Legs['ManeuverBearings']
+#     global ManeuverLongLat
+#     ManeuverLongLat= Legs['ManeuverLongLat']
+#     global TotalSteps
+#     TotalSteps= Legs['TotalSteps']
+#     test()
+#     #app = firebase.FirebaseApplication('https://augv-c7210.firebaseio.com/',None)
+# =============================================================================
     
-    schedule.every(1).seconds.do(job)
-    while 1:
-        schedule.run_pending()
-        time.sleep(0)
-        p=job()
-        compareUUID(p)
-        #l=[]
-        #l.append(p)
-        #print("P in f is",p)
+    
 
-def compareUUID(x):
-    
-    #print("UUID in Compare is",x)
-    UUID.insert(0,x)
-    if len(UUID)>2:
-        del UUID[2:]
-        if(UUID[0]==UUID[1]):
-            print("No change in route")
-        else:
-            print("Route Changed")
-            parseNewRouteData()
+# =============================================================================
+# def compareUUID(x):
+#     print("Compare UUID is Running")
+#     #print("UUID in Compare is",x)
+#     UUID.insert(0,x)
+#     print("UUID")
+#     if len(UUID)>2:
+#         del UUID[2:]
+#         if(UUID[0]==UUID[1]):
+#             print("No change in route")
+#         else:
+#             print("Route Changed")
+#             parseNewRouteData()
+# =============================================================================
     #for x in range(len(UUID)): 
         #print("UUID array is",UUID[x])  
-
-def parseNewRouteData():
-    print("Parsing New Route")
+def test():
+    print('Global Variable',TotDistance)
+    
+def ParseRouteForFirstTime():
+    print("Total distance")
+    Routes = app.get('/json','routes')
+    Legs = Routes['legs']
+    global TotDistance
+    TotDistance= Legs['distance']
+    global IndividualStepDistance
+    IndividualStepDistance= Legs['IndividualStepDistance']
+    global Instructions
+    Instructions= Legs['Instructions']
+    global ManeuverBearings
+    ManeuverBearings= Legs['ManeuverBearings']
+    global ManeuverLongLat
+    ManeuverLongLat= Legs['ManeuverLongLat']
+    global TotalSteps
+    TotalSteps= Legs['TotalSteps']
+    test()
+    
+    #Routes = app.get('/json','routes')
+    
+    
+    #a=getDistance()
+    #print(a)
+            
         
-f()
-  
+def main():
+#    print("This is main function")
+#    print("Parse the route first time")
+   # app = firebase.FirebaseApplication('https://augv-c7210.firebaseio.com/',None)
+   #assignApp(app)
+    #parseRouteData()
+    ParseRouteForFirstTime()
+#    f()
+    
+if __name__ == "__main__":
+   main()
+     
 
+#print(k)
