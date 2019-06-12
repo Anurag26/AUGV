2eba18aeb3d76497900d57486a26f966386789fa
Anurag Bannur <bannuranurag@gmail.com>
2019-06-12 23:58:03 +0530

    Working, tested on local server with port 86

--- move_forward1.py
+++ move_forward1.py
@@ -15,7 +15,8 @@ Instructions=""
 ManeuverBearings=""
 ManeuverLongLat=""
 TotalSteps=""
-
+global step_counter
+step_counter = 1
 servoPIN = 13
 motorpin = 21
 GPIO.setmode(GPIO.BCM)
@@ -27,83 +28,84 @@ motor_pwm = GPIO.PWM(motorpin, 50)
 motor_pwm.start(0)
 servo_pwm.start(6.25)
 
-global counter
-counter = 0
-global distance
-distance = 0
-global delta
-delta = 0
-global flag
-flag = 1
-step_counter = 1
-maxandmin = mag_calibr.calibrate_the_compass()
-magnetometerServo.update(maxandmin)
-app = firebase.FirebaseApplication('https://augv-c7210.firebaseio.com/',None)
-#result = app.get('/json','routes')
-#print(result)
+def start():
+    global counter
+    counter = 0
+    global distance
+    distance = 0
+    global delta
+    delta = 0
+    global flag
+    flag = 1
+    step_counter = 1
+    maxandmin = mag_calibr.calibrate_the_compass()
+    magnetometerServo.update(maxandmin)
+    app = firebase.FirebaseApplication('https://augv-c7210.firebaseio.com/',None)
+    #result = app.get('/json','routes')
+    #print(result)
 
-Routes = app.get('/json','routes')
-Legs = Routes['legs']
-ManeuverLongLat = Legs['ManeuverLongLat']
-ManeuverBearings = Legs['ManeuverBearings']
-NumberOfSteps = Legs["TotalSteps"]
-print(ManeuverLongLat)
-#print(ManeuverBearings)
-#print(NumberOfSteps)
-def compare_value(step_count):
-    #try:
-    current_lat = 0
-    current_lon = 0
-    global distance_threshold
-    distance_threshold= 2 
-    '''while current_lat == 0 and current_lon == 0:
+    Routes = app.get('/json','routes')
+    Legs = Routes['legs']
+    ManeuverLongLat = Legs['ManeuverLongLat']
+    ManeuverBearings = Legs['ManeuverBearings']
+    NumberOfSteps = Legs["TotalSteps"]
+    print(ManeuverLongLat)
+    #print(ManeuverBearings)
+    #print(NumberOfSteps)
+    def compare_value(step_count):
+        #try:
+        current_lat = 0
+        current_lon = 0
+        global distance_threshold
+        distance_threshold= 2 
+        '''while current_lat == 0 and current_lon == 0:
+            motor_pwm.ChangeDutyCycle(0)
+            latlongs=gpss.gpsloatlong()
+            current_lat = latlongs[0]
+            current_lon = latlongs[1]'''
+        global step_counter
+        if step_counter < step_count+1:
+            target_lat = ManeuverLongLat[step_counter * 2]
+            target_lon = ManeuverLongLat[step_counter * 2 + 1]
+            #distance = math.sqrt((target_lat - current_lat)**2 + (target_lon - current_lon)**2)*111000
+            
+        
+        #print(ManeuverBearings[step_counter - ])
+    #    print(delta)
+        #gpio.setup(13,gpio.OUT)
+        #gpio.output(13, True)
+        #d1 = compare_value()
+        
+            #print("Move Forward ", distance)
+            #print("Firebase bearings: ", ManeuverBearings[step_counter - 1])
+            #current_bearings = magnetometerServo.bearings()
+        print("calling servo_test")
+        servo_test.servo_testing(target_lat,target_lon,servo_pwm,motor_pwm,ManeuverBearings[step_counter-1])       
+            #motor_pwm.ChangeDutyCycle(50)
+            #gpio.output(21, gpio.HIGH)
+            #print("Face ", b)
+            
+            
+        print("Reached destination", distance)
         motor_pwm.ChangeDutyCycle(0)
-        latlongs=gpss.gpsloatlong()
-        current_lat = latlongs[0]
-        current_lon = latlongs[1]'''
-    global step_counter
-    if step_counter < step_count+1:
-        target_lat = ManeuverLongLat[step_counter * 2]
-        target_lon = ManeuverLongLat[step_counter * 2 + 1]
-        #distance = math.sqrt((target_lat - current_lat)**2 + (target_lon - current_lon)**2)*111000
-        
-    
-    #print(ManeuverBearings[step_counter - ])
-#    print(delta)
-    #gpio.setup(13,gpio.OUT)
-    #gpio.output(13, True)
-    #d1 = compare_value()
-    
-        #print("Move Forward ", distance)
-        #print("Firebase bearings: ", ManeuverBearings[step_counter - 1])
-        #current_bearings = magnetometerServo.bearings()
-    print("calling servo_test")
-    servo_test.servo_testing(target_lat,target_lon,servo_pwm,motor_pwm,ManeuverBearings[step_counter-1])       
-        #motor_pwm.ChangeDutyCycle(50)
-        #gpio.output(21, gpio.HIGH)
-        #print("Face ", b)
-        
-        
-    print("Reached destination", distance)
-    motor_pwm.ChangeDutyCycle(0)
-    step_counter = step_counter + 1
-    print("Step counter incremented to ", step_counter)
-    if step_counter == step_count + 1:
-        print("Navigation complete")
-        #gpio.cleanup()
-        motor_pwm.ChangeDutyCycle(0)
-       # schedule.clear('nav')
-        #print("Schedule cleared")            
+        step_counter = step_counter + 1
+        print("Step counter incremented to ", step_counter)
+        if step_counter == step_count + 1:
+            print("Navigation complete")
+            #gpio.cleanup()
+            motor_pwm.ChangeDutyCycle(0)
+           # schedule.clear('nav')
+            #print("Schedule cleared")            
 
-    #except:
-    #    print("Exception occured in compare_value")
+        #except:
+        #    print("Exception occured in compare_value")
 
-#schedule.every(2).seconds.do(compare_value, NumberOfSteps).tag('nav')
+    #schedule.every(2).seconds.do(compare_value, NumberOfSteps).tag('nav')
 
-while step_counter <= NumberOfSteps + 1:
-    compare_value(NumberOfSteps)
-    # Checks whether a scheduled task
-    # is pending to run or not
- #   schedule.run_pending()
+    while step_counter <= NumberOfSteps + 1:
+        compare_value(NumberOfSteps)
+        # Checks whether a scheduled task
+        # is pending to run or not
+     #   schedule.run_pending()
 #time.sleep(1)
 
